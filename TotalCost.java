5ad0c67cadcab3a34decfe2f65940f53bf4e522f
gunchab <guncharahul@gmail.com>
2015-04-27 17:41:37 -0700

    Added Comments

--- TotalCost.java
+++ TotalCost.java
@@ -1,8 +1,13 @@
-
+/*
+ * This is a class that has a method to calculate the total cost of an item.
+ */
 public class TotalCost {
 	
-	final String NECESSORY = "Necessary";
-	final String lUXURY = "Luxury";
+	private final String NECESSORY = "Necessary";
+	private final String lUXURY = "Luxury";
+	/*
+	 * This method takes the amount of the item and the item type and returns the final cost.
+	 */
 	public int totalCostCalculator(int amount,String type)
 	{
 		int total= 0;
